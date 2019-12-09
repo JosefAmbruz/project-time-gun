a73deea837fc7c63081a48a83226dfda3e51b8dc
Josef Ambruz <ambruz.josef03@gmail.com>
2019-12-09 19:34:49 +0100

    Implemented 2D camera

--- Time Gun/src/Actors/Player.gd
+++ Time Gun/src/Actors/Player.gd
@@ -2,25 +2,6 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 export var speed = 300
-#export var speed: = 300
-#var velocity = Vector2()
-#
-#func get_input():
-#	velocity = Vector2()
-#	look_at(get_global_mouse_position())
-#	if Input.is_action_pressed("right"):
-#		velocity.x += 1
-#	if Input.is_action_pressed("left"):
-#		velocity.x -= 1
-#	if Input.is_action_pressed("up"):
-#		velocity.y -= 1
-#	if Input.is_action_pressed("down"):
-#		velocity.y += 1
-#	velocity = velocity.normalized() * speed
-#
-#func _physics_process(delta: float) -> void:
-#	get_input()
-#	velocity = move_and_slide(velocity)
 
 func get_direction():
 	var direction: = Vector2(
